cabd952689f517aa4f7612164b8fc1cb4b780bef
StOil-L <lorenzo.puccio2@gmail.com>
2025-05-21 17:02:41 +0200

    Added portfolio header component + temporary site logo

---
+++ src/components/PortfolioHeader.tsx
@@ -0,0 +1,13 @@
+type PortfolioHeaderProps = {
+    children: string
+};
+
+function PortfolioHeader(props : PortfolioHeaderProps) {
+    return (
+        <header>
+            <h1>{props.children}</h1>
+        </header>
+    )
+}
+
+export default PortfolioHeader
