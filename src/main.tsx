485d2d0788a21baaa3097b6632fa6b1b2e65f3aa
StOil-L <lorenzo.puccio2@gmail.com>
2025-06-06 14:38:02 +0200

    Moved CSS files to stylesheets folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './stylesheets/index.css'
 import RouteConfig from "./components/RouteConfig.tsx";
 
 createRoot(document.getElementById('root')!).render(
