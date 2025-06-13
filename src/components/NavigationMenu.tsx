f399eda5effb4d6b14a763f71997ecad1582a641
StOil-L <lorenzo.puccio2@gmail.com>
2025-06-13 13:20:36 +0200

    Minor changes
    - added nav tag to NavigationMenu.tsx
    - added "brand" icons from FontAwesome in package.json

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -3,12 +3,12 @@ import DarkModeToggle from "./DarkModeToggle.tsx";
 
 function NavigationMenu() {
   return (
-    <>
+    <nav>
       <Link to="aboutme">A propos de moi</Link> {'\t'}
       <Link to="projects">Projets</Link> {'\t'}
       <Link to="certifications">Certifications</Link> {'\t'}
       <DarkModeToggle />
-    </>
+    </nav>
   )
 }
 
