6671e076a026acad1e6097892f2632a5a7ae7c87
StOil-L <lorenzo.puccio2@gmail.com>
2025-06-26 22:44:47 +0200

    First implementation of EducationSummary component
    - based on current ProjectSummary implementation
    - Technology component and stylesheet amended

---
+++ src/components/EducationSummary.tsx
@@ -0,0 +1,56 @@
+import '../stylesheets/EducationSummary.css';
+import Technology, {type TechnologyProps} from "./Technology.tsx";
+
+export interface EducationSummaryProps {
+  title: string,
+  imgsrc?: string,
+  school?: string,
+  city?: string,
+  description: string,
+  technologies?: TechnologyProps[],
+  results?: string,
+  obtentionDate?: string | Date;
+  link?: string,
+  isAlt?: boolean,
+}
+
+const localDateOptions: Intl.DateTimeFormatOptions = {
+  day: "numeric",
+  month: "long",
+  year: "numeric"
+};
+
+function EducationSummary(props: EducationSummaryProps) {
+  return (
+    <div className={`education-summary${props.isAlt ? ' alt' : ''}`}>
+      <section>
+        <h2>{props.title}</h2>
+        {props.school && (<h3>{props.school}{props.city && `, ${props.city}`}</h3>)}
+        {props.obtentionDate && (
+          <h4>Obtenu le {typeof props.obtentionDate === 'string'?
+            props.obtentionDate : (props.obtentionDate as Date)
+              .toLocaleDateString(undefined, localDateOptions)}</h4>
+        )}
+        {props.imgsrc && (
+          <img src={props.imgsrc} alt={`Logo ${props.school}`}/>
+        )}
+        <p>{props.description}</p>
+      </section>
+      {props.technologies && (
+        <>
+          <h3>Technologies enseignées</h3>
+          <div className="technologies">
+            {props.technologies.map((technology: TechnologyProps, i: number) => {
+              return <Technology key={i} name={technology.name} icon={technology.icon}
+                                 color={technology.color} highlight={technology.highlight} />
+            })}
+          </div>
+        </>
+      )}
+      {props.results && (<h4>Résultats : {props.results}</h4>)}
+      {props.link && (<a href={props.link}>En savoir plus</a>)}
+    </div>
+  )
+}
+
+export default EducationSummary;
--- src/components/pages/EducationPage.tsx
+++ src/components/pages/EducationPage.tsx
@@ -1,8 +1,21 @@
 import PortfolioHeader from "../layouts/PortfolioHeader.tsx";
+import {manualReverse} from "../../util/misc.ts";
+import {education} from "../../assets/Education.ts";
+import EducationSummary, {type EducationSummaryProps} from "../EducationSummary.tsx";
 
 function EducationPage() {
   return (
-    <PortfolioHeader>Formation</PortfolioHeader>
+    <>
+      <PortfolioHeader>Formation</PortfolioHeader>
+      {(manualReverse(education) as EducationSummaryProps[])
+        .map((education, i: number) => {
+        return <EducationSummary key={i} title={education.title} description={education.description}
+                                 imgsrc={education.imgsrc} school={education.school} city={education.city}
+                                 technologies={education.technologies} results={education.results}
+                                 obtentionDate={education.obtentionDate} link={education.link}
+                                 isAlt={i % 2 == 0} />
+      })}
+    </>
   )
 }
 
