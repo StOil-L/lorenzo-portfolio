2c8b141c5a46c1e47ad7905e28cfdd58c284edf8
StOil-L <lorenzo.puccio2@gmail.com>
2025-07-24 16:36:36 +0200

    Added nullable imgalt property to PortfolioHeader props

--- src/components/layouts/PortfolioHeader.tsx
+++ src/components/layouts/PortfolioHeader.tsx
@@ -3,13 +3,14 @@ import '../../stylesheets/PortfolioHeader.css';
 type PortfolioHeaderProps = {
   children: string
   imgsrc: string
+  imgalt?: string
 };
 
 function PortfolioHeader(props : PortfolioHeaderProps) {
   return (
     <header>
       <h1>{props.children.toUpperCase()}</h1>
-      <img src={props.imgsrc} alt={`Section ${props.children}`} />
+      <img src={props.imgsrc} alt={props.imgalt ? props.imgalt : `Section ${props.children}`} />
     </header>
   )
 }
