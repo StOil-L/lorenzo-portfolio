205cf53eab227f042e88e5beff22166a1fbc3a3b
StOil-L <lorenzo.puccio2@gmail.com>
2025-07-29 12:11:07 +0200

    First implementation of HomePage component
    - includes CarouselWrapper component which uses HomePageNavigation array, redirects to other
    - I might want to add more text afterwards

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,38 +1,40 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import '../../stylesheets/HomePage.css'
 import PortfolioHeader from "../layouts/PortfolioHeader.tsx";
 import CarouselWrapper from "../carousel/CarouselWrapper.tsx";
-import {testCarousel} from "../../assets/carousel/TestCarousel.ts";
+import {useEffect} from "react";
+import {homePageNavigation} from "../../assets/carousel/HomePageNavigation.ts";
 
 function HomePage() {
-  // const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }, []);
 
   return (
     <>
-      <PortfolioHeader imgsrc="lorenzo-portfolio/photos/homepage.jpg">Lorenzo PUCCIO</PortfolioHeader>
-      {/*<div>*/}
-      {/*  <a href="https://vite.dev" target="_blank">*/}
-      {/*    <img src={viteLogo} className="logo" alt="Vite logo" />*/}
-      {/*  </a>*/}
-      {/*  <a href="https://react.dev" target="_blank">*/}
-      {/*    <img src={reactLogo} className="logo react" alt="React logo" />*/}
-      {/*  </a>*/}
-      {/*</div>*/}
-      {/*<h1>Vite + React</h1>*/}
-      {/*<div className="card">*/}
-      {/*  <button onClick={() => setCount((count) => count + 1)}>*/}
-      {/*    count is {count}*/}
-      {/*  </button>*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/HomePage.tsx</code> and save to test HMR*/}
-      {/*  </p>*/}
-      {/*</div>*/}
-      {/*<p className="read-the-docs">*/}
-      {/*  Click on the Vite and React logos to learn more*/}
-      {/*</p>*/}
-      <CarouselWrapper slides={testCarousel} />
+      <PortfolioHeader imgsrc="lorenzo-portfolio/photos/homepage.jpg" imgalt="Page d'accueil">
+        Lorenzo PUCCIO
+      </PortfolioHeader>
+      <div id="page-info-transition" />
+      <div id="page-info">
+        <div id="explanation">
+          <h3>Bienvenue sur mon portfolio !</h3>
+          <p>Je m'appelle Lorenzo PUCCIO, j'ai 23 ans et je suis développeur web diplômé d'un Master Génie Logiciel à la Faculté des Sciences de Montpellier. Bien que je sois opérationnel en développement front-end avec TypeScript et React, j'ai une appétence particulière pour le développement back-end, notamment avec Java et Spring Boot. Je me considère comme quelqu'un d'authentique, qui sait faire preuve de persévérance et de rigueur malgré une certaine introversion.</p>
+          <p>Ayant actuellement 8 mois d'expérience en développement logiciel, je suis aujourd'hui à la recherche de deux opportunités dans les villes de Montpellier, Paris, Lyon, Toulouse et Nantes, classées ici par ordre d'importance :</p>
+          <ol>
+            <li>Une opportunité en CDI / CDD en développement back-end, de préférence avec Java et Spring Boot ou en développement front-end, de préférence avec TypeScript et React</li>
+            <li>Une opportunité en alternance de 14 mois pour septembre 2025 en développement web et / ou mobile</li>
+          </ol>
+          <p>Au plaisir d'échanger avec vous sur de telles opportunités !</p>
+          <p><b>Explorez les différentes catégories de mon portfolio en utilisant la barre de navigation au sommet de la page ou en utilisant les liens présents dans le carousel d'images ci-dessous :</b></p>
+          <center>
+            <div id="homepage-carousel">
+              <CarouselWrapper slides={homePageNavigation} />
+            </div>
+          </center>
+          <br />
+        </div>
+      </div>
     </>
   )
 }
