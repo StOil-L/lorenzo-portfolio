cd787acf35814b1f350a7db0af8ef69bcc671b15
StOil-L <lorenzo.puccio2@gmail.com>
2025-08-29 14:34:37 +0200

    Simplified ContactPage component

--- src/components/pages/ContactPage.tsx
+++ src/components/pages/ContactPage.tsx
@@ -14,13 +14,13 @@ function ContactPage() {
     }} route="contact">
       <div id="explanation">
         <b>
-          <h3>Besoin d'un renseignement ou envie de faire un retour sur mon parcours, mes réalisations ou ce site web ? Contactez-moi !</h3>
-          <p>Je suis généralement disponible du lundi au vendredi de 10h à 12h, puis de 14h à 17h. Vous pouvez me joindre via mon profil LinkedIn, mon adresse mail ou mon numéro de téléphone.</p>
+          <h3>Besoin d'un renseignement ou envie de faire un retour sur mon parcours, mes réalisations ou ce site web ? Contactez-moi via les liens ci-dessous :</h3>
+        {/*  <p>Je suis généralement disponible du lundi au vendredi de 10h à 12h, puis de 14h à 17h. Vous pouvez me joindre via mon profil LinkedIn, mon adresse mail ou mon numéro de téléphone.</p>*/}
         </b>
-        <p>Je ne serai cependant <b>pas disponible</b> pour un appel téléphonique aux dates suivantes :</p>
-        <ul>
-          <li><b>Jeudi 21 août</b> après-midi</li>
-        </ul>
+        {/*<p>Je ne serai cependant <b>pas disponible</b> pour un appel téléphonique aux dates suivantes :</p>*/}
+        {/*<ul>*/}
+        {/*  <li><b>Jeudi 21 août</b> après-midi</li>*/}
+        {/*</ul>*/}
         <div className="contacts">
           <ContactButton baseHref={"https://www.linkedin.com/in/"} contactType={"linkedin"}
                          icon={faLinkedin} isWebsite={true}/>
