cfc30bd0d19100544fe6036c2102a651afd863b8
StOil-L <lorenzo.puccio2@gmail.com>
2025-08-29 14:29:50 +0200

    Simplified Interest component

--- src/components/summary/Interest.tsx
+++ src/components/summary/Interest.tsx
@@ -5,20 +5,21 @@ import type {CarouselSlideProps} from "../carousel/CarouselSlide.tsx";
 export interface InterestProps {
   id: string,
   name: string,
-  paragraphs: string | string[],
+  // paragraphs: string | string[],
   slides: CarouselSlideProps[],
-  isAlt?: boolean,
+  // isAlt?: boolean,
+  displayTitle?: boolean,
 }
 
 function Interest(props: InterestProps) {
 
   return (
-    <div className={`summary${props.isAlt ? " alt" : ""}`} id={props.id}>
-      <h2>{props.name}</h2>
-      {Array.isArray(props.paragraphs) ? props.paragraphs.map((paragraph, i) => {
-        return <p key={i}>{paragraph}</p>
-      }) : props.paragraphs}
-      <CarouselWrapper slides={props.slides} sideDisplay={true} textSide={props.isAlt? "left" : "right"} />
+    <div className="interest" id={props.id}>
+      {props.displayTitle && <h2>{props.name}</h2>}
+      {/*{Array.isArray(props.paragraphs) ? props.paragraphs.map((paragraph, i) => {*/}
+      {/*  return <p key={i}>{paragraph}</p>*/}
+      {/*}) : props.paragraphs}*/}
+      <CarouselWrapper slides={props.slides} />
     </div>
   )
 }
